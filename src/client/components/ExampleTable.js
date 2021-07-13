1ce2a8470711541d6eab144d0cd08d039fe6115d
samfedenia <sam@fedenia.xyz>
2021-07-13 11:11:23 -0500

    fix example table

--- src/client/components/ExampleTable.js
+++ src/client/components/ExampleTable.js
@@ -5,24 +5,28 @@ const ExampleTable = () => {
     <div style={{ width: "80vw", paddingTop: "2rem" }}>
       <h2>Example CSV file format</h2>
       <table style={{ width: "100%" }}>
-        <tr>
-          <th>TIME</th>
-          <th>Dependent variable 1</th>
-          <th>Dependent variable 2</th>
-          <th>...</th>
-        </tr>
-        <tr>
-          <td>date-time</td>
-          <td>number</td>
-          <td>number</td>
-          <td>...</td>
-        </tr>
-        <tr>
-          <td>...</td>
-          <td>...</td>
-          <td>...</td>
-          <td>...</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>TIME</th>
+            <th>Dependent variable 1</th>
+            <th>Dependent variable 2</th>
+            <th>...</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>date-time</td>
+            <td>number</td>
+            <td>number</td>
+            <td>...</td>
+          </tr>
+          <tr>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+            <td>...</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
