cf04a75424b41ccebc66a4549ff09115e7ba6d55
samfedenia <sam@fedenia.xyz>
2021-08-13 12:55:57 -0500

    styling tables

--- src/client/components/CSVTable.js
+++ src/client/components/CSVTable.js
@@ -1,15 +1,35 @@
-import React, { useState } from "react";
+import React from "react";
+import {makeStyles} from '@material-ui/core/styles'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    maxWidth: '75vw',
+    backgroundColor: theme.palette.background.paper,
+    position: 'relative',
+    overflow: 'auto',
+    maxHeight: 350,
+    margin: '1rem'
+  },
+}));
 
 export default ({ data }) => {
+  const classes = useStyles()
   return (
-    <>
-      {data.map((row, idx) => {
-        if (idx <= 25) {
-          return <p key={idx}>{row.data}</p>;
-        }
-      })}
-      <p>...</p>
-      <p>...</p>
-    </>
+    
+    <List className={classes.root} subheader={<li />}>
+        {data.map((item, idx) => {
+          if (idx <= Math.floor(data.length*0.25)) {
+            return (<ListItem key={`item-${idx}`}>
+            <ListItemText primary={`${item.data}`} />
+          </ListItem>
+            )
+          }
+        })
+      }
+</List>
   );
 };
--- src/client/components/ExampleTable.js
+++ src/client/components/ExampleTable.js
@@ -1,33 +1,50 @@
-import React from "react";
+import React from 'react';
+import { useTheme } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+function createData(x, y1, y2, y3, y4) {
+  return { x, y1, y2, y3, y4 };
+}
+
+const rows = [
+  createData('date-time', 'number', 'number', 'number', '...'),
+  createData('...', '...', '...', '...', '...'),
+];
 
 export default () => {
-  return (
-    <div style={{ width: "80vw", paddingTop: "2rem" }}>
-      <h2>Example CSV file format</h2>
-      <table style={{ width: "100%" }}>
-        <thead>
-          <tr>
-            <th>TIME</th>
-            <th>Dependent variable 1</th>
-            <th>Dependent variable 2</th>
-            <th>...</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>date-time</td>
-            <td>number</td>
-            <td>number</td>
-            <td>...</td>
-          </tr>
-          <tr>
-            <td>...</td>
-            <td>...</td>
-            <td>...</td>
-            <td>...</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-};
+const classes = useTheme();
+
+return (
+  
+    <Table className={classes.table} style={{maxWidth: '75vw', paddingTop: '2rem'}} size="small" aria-label="simple table">
+      <caption>^Example of input csv data</caption>
+      <TableHead>
+        <TableRow>
+          <TableCell>Date-Time</TableCell>
+          <TableCell align="right">Dependent Variable 1</TableCell>
+          <TableCell align="right">Dependent Variable 2</TableCell>
+          <TableCell align="right">Dependent Variable 3</TableCell>
+          <TableCell align="right">...</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {rows.map((row, idx) => (
+          <TableRow key={idx}>
+            <TableCell component="th" scope="row">
+              {row.x}
+            </TableCell>
+            <TableCell align="right">{row.y1}</TableCell>
+            <TableCell align="right">{row.y2}</TableCell>
+            <TableCell align="right">{row.y3}</TableCell>
+            <TableCell align="right">{row.y4}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+
+);
+}
