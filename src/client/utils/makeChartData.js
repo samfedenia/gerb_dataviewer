84f742797275afd5076dc0849e5404c0515e707d
samfedenia <sam@fedenia.xyz>
2021-08-13 13:24:55 -0500

    handle 4 digit year input data

--- src/client/utils/makeChartData.js
+++ src/client/utils/makeChartData.js
@@ -1,11 +1,14 @@
 import * as d3 from "d3";
-const parser = d3.timeParse("%m/%d/%y %H:%M:%S.%f");
+let parser = d3.timeParse("%m/%d/%y %H:%M:%S.%f");
 
 const makeChartData = (rawData) => {
   const headers = rawData.shift(1);
   const output = [];
   for (let i = 1; i < headers.length; i++) {
     const column = rawData.map((row) => [row[0], row[i]]);
+    if (!parser(column[0][0].slice(0,-3))) {
+      parser = d3.timeParse("%m/%d/%Y %H:%M:%S.%f");
+    }
     const data = {
       id: headers[i],
       data: column.map(([x, y]) => ({
